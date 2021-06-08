91a9ed8aba1007eefbec9abc9aaf825128ebafa9
Luis Perez <ljpm9401@gmail.com>
2021-06-08 13:23:34 -0500

    video introduccion

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,7 +11,7 @@
     
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/Qk5LS8GRcAk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     p.mt-4 Apreciado participante, bienvenido a este momento de aprendizaje orientado a la identificación de las características de la educación STEM y sus actividades de aprendizaje, en este sentido se le sugiere revisar la siguiente infografía para reconocer el contexto de aprendizaje. Adelante. 
       
